52adeb90c92f0c107782d5cf6618b55f3439de07
Rhylhynx <sj2.rbuenaventura26@joysistvi.edu.ph>
2026-06-21 18:45:38 +0800

    initial commit

---
+++ src/Day2/discussion/SampleCode.java
@@ -0,0 +1,17 @@
+
+package Day2.discussion;
+
+public class SampleCode {
+ public static void main (String[] args) { // main method
+    System.out.println("Hello World"); // print statement 
+ }           
+}
+/*
+    2 Types of Comments
+    1. Single Line Comment
+    2. Multi Line Comment
+*/
+// Main Method (pvsm) > starting point of programs execution 
+// Print Statement (sout) > it tells the computer to print 
+
+
---
+++ src/Day3/Discussion/SampleCode.java
@@ -0,0 +1,15 @@
+
+package Day3.Discussion; // package
+
+public class SampleCode { // class
+ public static void main (String[] args) { // main method
+    System.out.println("Hello World"); // print statement 
+ }           
+}
+
+// Syntax > Structure of Error 
+// Syntax Rule > Syntax Error 
+// Main Method (pvsm + tab) > Starting point of program execution 
+
+
+
