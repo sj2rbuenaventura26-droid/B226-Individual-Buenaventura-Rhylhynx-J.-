4a95d001c224f6cad68d78f02b407dcc99b98acd
Rhylhynx <sj2.rbuenaventura26@joysistvi.edu.ph>
2026-06-23 11:08:49 +0800

    Added new Day4

---
+++ src/Day4/Discussion/Datatype.java
@@ -0,0 +1,19 @@
+
+package Day4.Discussion;
+
+public class Datatype {
+    public static void main (String[] args) {
+       boolean isActive = true;
+        char letter = 'A';
+        byte num = 50;
+        short num2 = 200;
+        int num3 = 10_000_000;
+        long num4 = 100_000_000_000l; // suffix 1/L
+        float temp = 35.50f;  // suffix f/F
+        double average = 98.01230429;
+        String word = "Java";
+        
+        
+                
+    }
+}
---
+++ src/Day4/Discussion/Variable.java
@@ -0,0 +1,56 @@
+
+package Day4.Discussion;
+
+
+public class Variable {
+    public static void main(String[] args) {
+         String firstName , middleName , lastName ;
+            firstName = "Rhylhynx";
+            middleName = "Jimenez";
+            lastName = "Buenaventura" ;
+            
+            System.out.println(firstName  + middleName  + lastName);
+            
+        
+        
+        
+        
+        
+         //initialization
+        // DT   var      value 
+        String timba = "Tubig";
+       
+       // bigas
+        timba = "bigas";
+        
+        // Printing a variable
+       //system.out
+        
+        
+        // 22, 20,18,24,26
+        int age = 22, age2 = 20,age3 = 18, age4 = 24;
+        
+       
+        System.out.println(age4);
+        // Age: 22
+        // concatenation (+) -> join values
+            
+ 
+
+        // 38.5
+        // declaration
+        double temp;
+        
+        //assignment
+        temp = 38.5;
+         
+        
+       
+           
+            
+               
+    
+                
+        
+    }
+}
